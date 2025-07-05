b757db1719ae408a6d804947c7546f2d80fa6f0d
nkd1512 <ddd.ddd201503@gmail.com>
2025-07-05 16:09:49 +0000

    Add welcome message, button, and icon to homepage

---
+++ my-quasar-app/src/pages/IndexPage.vue
@@ -0,0 +1,22 @@
+<template>
+  <q-page class="flex flex-center">
+    <img
+      alt="Quasar logo"
+      src="~assets/quasar-logo-vertical.svg"
+      style="width: 200px; height: 200px"
+    />
+
+    <h1>ยินดีต้อนรับสู่แอป Quasar ของฉัน!</h1>
+    <p>เรามาสร้างสรรค์สิ่งดีๆ กัน!</p>
+
+    <q-btn color="primary" label="คลิกฉันสิ!" icon="touch_app" class="q-mt-md" />
+    <q-icon name="favorite" size="lg" color="red" class="q-mt-md" />
+
+  </q-page>
+</template>
+
+<script setup>
+</script>
+
+<style scoped>
+</style>
