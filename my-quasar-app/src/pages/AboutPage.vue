bfb77b9d4e24d11b2b903af8149894917edfea43
nkd1512 <ddd.ddd201503@gmail.com>
2025-07-05 16:52:45 +0000

    Add About page and navigation

---
+++ my-quasar-app/src/pages/AboutPage.vue
@@ -0,0 +1,19 @@
+<template>
+  <q-page class="flex flex-center">
+    <div>
+      <h1>เกี่ยวกับแอปพลิเคชันของเรา</h1>
+      <p>นี่คือหน้าเกี่ยวกับเรา ที่คุณสามารถใส่ข้อมูลเกี่ยวกับแอปหรือบริษัทของคุณได้</p>
+      <q-btn label="กลับหน้าหลัก" color="secondary" @click="$router.push('/')" />
+    </div>
+  </q-page>
+</template>
+
+<script setup>
+</script>
+
+<style scoped>
+div {
+  text-align: center;
+  padding: 20px;
+}
+</style>
--- my-quasar-app/src/router/routes.js
+++ my-quasar-app/src/router/routes.js
@@ -7,6 +7,14 @@ const routes = [
     ]
   },
 
+  {
+    path: '/about',
+    component: () => import('layouts/MainLayout.vue'),
+    children: [
+      { path: '', component: () => import('pages/AboutPage.vue') }
+    ]
+  },
+
   // Always leave this as last one,
   // but you can also remove it
   {
