e12bf1221dddb115fdc5e8141c16f7f2931d4fd8
nkd1512 <ddd.ddd201503@gmail.com>
2025-07-06 08:50:46 +0000

    Update MainLayout.vue with new links

--- my-quasar-app/src/layouts/MainLayout.vue
+++ my-quasar-app/src/layouts/MainLayout.vue
@@ -12,7 +12,7 @@
         />
 
         <q-toolbar-title>
-          Quasar App
+          แอปของฉัน
         </q-toolbar-title>
 
         <div>Quasar v{{ $q.version }}</div>
@@ -27,12 +27,13 @@
       <q-list>
         <q-item-label
           header
+          class="text-grey-8"
         >
           Essential Links
         </q-item-label>
 
         <EssentialLink
-          v-for="link in linksList"
+          v-for="link in essentialLinks"
           :key="link.title"
           v-bind="link"
         />
@@ -42,4 +43,67 @@
     <q-page-container>
       <router-view />
     </q-page-container>
-  <
+  </q-layout>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+import EssentialLink from 'components/EssentialLink.vue'
+
+const essentialLinks = [
+  {
+    title: 'Docs',
+    caption: 'quasar.dev',
+    icon: 'school',
+    link: 'https://quasar.dev'
+  },
+  {
+    title: 'Github',
+    caption: 'github.com/quasarframework',
+    icon: 'code',
+    link: 'https://github.com/quasarframework'
+  },
+  {
+    title: 'Discord Chat',
+    caption: 'chat.quasar.dev',
+    icon: 'chat',
+    link: 'https://chat.quasar.dev'
+  },
+  {
+    title: 'Forum',
+    caption: 'forum.quasar.dev',
+    icon: 'record_voice_over',
+    link: 'https://forum.quasar.dev'
+  },
+  {
+    title: 'Twitter',
+    caption: '@quasarframework',
+    icon: 'rss_feed',
+    link: 'https://twitter.quasar.dev'
+  },
+  {
+    title: 'Facebook',
+    caption: '@QuasarFramework',
+    icon: 'public',
+    link: 'https://facebook.quasar.dev'
+  },
+  {
+    title: 'Awesome Quasar',
+    caption: 'Community Quasar projects',
+    icon: 'favorite',
+    link: 'https://awesome.quasar.dev'
+  },
+  {
+    title: 'เกี่ยวกับเรา',
+    caption: 'ข้อมูลเกี่ยวกับแอป',
+    icon: 'info',
+    link: '/about'
+  }
+]
+
+const leftDrawerOpen = ref(false)
+
+function toggleLeftDrawer () {
+  leftDrawerOpen.value = !leftDrawerOpen.value
+}
+</script>
